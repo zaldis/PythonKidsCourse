f892b87c4648f4e1bcc70988101c3e6668ce9921
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2021-09-26 12:28:02 +0300

    Add second lesson

---
+++ module_examples/example1/main.py
@@ -0,0 +1,11 @@
+# from solution.math import PI
+# from solution.math import *
+
+import math
+from solution import math as my_math
+
+
+if __name__ == '__main__':
+    print(math.pi, math.e)
+
+    print(my_math.M)
